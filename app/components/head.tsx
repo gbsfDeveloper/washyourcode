7a74cec994463a1c2811b3a5193e9c076c0ab5f7
ggbsfgames <gbsfgames@gmail.com>
2023-08-19 23:36:28 -0600

    create customize header for project

---
+++ app/components/head.tsx
@@ -0,0 +1,14 @@
+
+import Head from 'next/head'
+ 
+function Header() {
+  return (
+    <div>
+      <Head>
+        <meta key="mobile-first" name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+    </div>
+  )
+}
+ 
+export default Header
